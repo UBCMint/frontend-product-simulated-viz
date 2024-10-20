f1aa9a3887e8fd5c9abb2a7df64cb7f843419058
ryanhan <ryanjhan10@gmail.com>
2024-10-20 00:20:29 -0700

    data and line chart

--- src/components/Visualizer/Visualizer.tsx
+++ src/components/Visualizer/Visualizer.tsx
@@ -38,6 +38,7 @@ export default function Visualizer() {
                     <SyncChart renderData={processedData} />
                 )}
             </div>
+            <DataTable renderData={processedData} />
         </div>
     );
 }
